c93f7fddd3b6cc44aa95d4106ced553945f516bd
Sahil Mattoo <42411417+sahilmattoo@users.noreply.github.com>
2025-11-25 10:02:52 +0530

    withoutWeek6

---
+++ Week3/CLI/project.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+## Call the operation.py file
+import operation as ops
+
+
+sum = ops.add(1,2)
+print(sum)
---
+++ Week3/CLI/utils.py
@@ -0,0 +1,5 @@
+import project
+import os
+
+add = project.add(1, 2)
+print(add)
