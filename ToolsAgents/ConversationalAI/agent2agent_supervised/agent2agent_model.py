2763092992671e07e32bc6cb62e56f8083c18b73
Sahil Mattoo <42411417+sahilmattoo@users.noreply.github.com>
2026-02-21 09:17:05 +0530

    update

--- ToolsAgents/ConversationalAI/agent2agent_supervised/agent2agent_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-
-def load_model_and_tokenizer(model_name: str = "t5-small"):
-    """Return (model, tokenizer) ready for training or inference."""
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    return model, tokenizer
